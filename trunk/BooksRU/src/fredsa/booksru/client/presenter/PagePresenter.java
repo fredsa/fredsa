5e67a574dbd9b6cf89821385a76083785eebec39
fredsa <fredsa@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2009-05-22 06:53:17 +0000

    storing lines, but need to refine query

--- trunk/BooksRU/src/fredsa/booksru/client/presenter/PagePresenter.java
+++ trunk/BooksRU/src/fredsa/booksru/client/presenter/PagePresenter.java
@@ -3,8 +3,10 @@ package fredsa.booksru.client.presenter;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import fredsa.booksru.client.BookService;
@@ -29,6 +31,7 @@ public class PagePresenter {
   private HTML waitingMessage;
 
   public PagePresenter(RootPanel rootPanel) {
+    rootPanel.add(new Label(line.getLineText()));
     waitingMessage = new HTML("Loading...");
     rootPanel.add(waitingMessage);
     rootPanel.add(pageView);
@@ -49,7 +52,13 @@ public class PagePresenter {
 
       public void onFailure(Throwable caught) {
         waitingMessage.setHTML(waitingMessage.getHTML() + ".");
-        requestLines(previousLine);
+        new Timer() {
+
+          @Override
+          public void run() {
+            requestLines(previousLine);
+          }
+        }.schedule(5000);
       }
 
       public void onSuccess(Line[] suggestedLines) {
--- trunk/BooksRU/src/fredsa/booksru/server/BookServiceImpl.java
+++ trunk/BooksRU/src/fredsa/booksru/server/BookServiceImpl.java
@@ -1,11 +1,15 @@
 package fredsa.booksru.server;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import fredsa.booksru.client.BookService;
@@ -45,9 +49,31 @@ public class BookServiceImpl extends RemoteServiceServlet implements BookService
     return new ArrayList<Book>(((List<Book>) query.execute()));
   }
 
+  private List<Line> getLines(PersistenceManager pm, Line previousLine) {
+    Query query = pm.newQuery(Line.class);
+    query.setFilter("previousLineNumber == " + previousLine.getLineNumber());
+    query.setRange(0, 10);
+    List<Line> results = (List<Line>) query.execute();
+    for (Iterator iterator = results.iterator(); iterator.hasNext();) {
+      Line line = (Line) iterator.next();
+      Log.debug(line.getCompoundKey());
+    }
+    return new ArrayList<Line>(results);
+  }
+
   public Line[] getLineSuggestions(Line previousLine) {
-    Line line1 = new Line(previousLine, "Once upon a time" + previousLine.getLineNumber());
-    Line line2 = new Line(previousLine, "It was a dark and stormy night");
-    return new Line[] {line1, line2,};
+    PersistenceManager pm = PMF.get().getPersistenceManager();
+    if (previousLine.getLineNumber() > 0) {
+      if (previousLine.getReads() == 0) {
+        pm.makePersistent(previousLine);
+      } else {
+        Key key = KeyFactory.createKey(Line.class.getSimpleName(), previousLine.getCompoundKey());
+        Line line = pm.getObjectById(Line.class, key);
+        line.incrementReads();
+        pm.makePersistent(line);
+      }
+    }
+    return getLines(pm, previousLine).toArray(new Line[] {});
   }
 }
+///0//-4129273751919394816/1//////
