b69ad64fc8da4afd65521fecfdab2feae1b2ecbc
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-11 21:27:14 +0000

    harcoded test case

--- trunk/fixhtml5/src/fixhtml5/client/Main.java
+++ trunk/fixhtml5/src/fixhtml5/client/Main.java
@@ -2,9 +2,13 @@ package fixhtml5.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import fixhtml5.shared.Case;
@@ -13,7 +17,43 @@ public class Main implements EntryPoint {
 
   private final CaseServiceAsync caseService = GWT.create(CaseService.class);
 
+  @Override
   public void onModuleLoad() {
+    // set uncaught exception handler
+    GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
+      @Override
+      public void onUncaughtException(Throwable throwable) {
+        String text = "Uncaught exception: ";
+        while (throwable != null) {
+          StackTraceElement[] stackTraceElements = throwable.getStackTrace();
+          text += throwable.toString() + "\n";
+          for (StackTraceElement stackTraceElement : stackTraceElements) {
+            text += "    at " + stackTraceElement + "\n";
+          }
+          throwable = throwable.getCause();
+          if (throwable != null) {
+            text += "Caused by: ";
+          }
+        }
+        DialogBox dialogBox = new DialogBox(true, false);
+        DOM.setStyleAttribute(dialogBox.getElement(), "backgroundColor", "#ABCDEF");
+        System.err.print(text);
+        text = text.replaceAll(" ", "&nbsp;");
+        dialogBox.setHTML("<pre>" + text + "</pre>");
+        dialogBox.center();
+      }
+    });
+
+    // use a deferred command so that the handler catches onModuleLoad2() exceptions
+    Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+      @Override
+      public void execute() {
+        onModuleLoad2();
+      }
+    });
+  }
+
+  private void onModuleLoad2() {
     final Element wait = Document.get().getElementById("wait");
     wait.setInnerHTML("Fetching cases...");
     caseService.getCase(0, new AsyncCallback<Case>() {
--- trunk/fixhtml5/src/fixhtml5/server/CaseServlet.java
+++ trunk/fixhtml5/src/fixhtml5/server/CaseServlet.java
@@ -14,8 +14,18 @@ public class CaseServlet extends RemoteServiceServlet implements CaseService {
   public Case getCase(int number) {
     c = new Case();
     c.setName("Simple case");
-    c.setHtml("this is <b> html </b>!");
-    c.setScript("alert(42)");
+    c.setHtml("<audio controls id=\"laser\" src=\"36846__EcoDTR__LaserRocket.mp3\"></audio><br/>"
+        + "<button onclick=\"play('laser')\">play</button>"
+        + "<div>Do not use the media controls. Instead, click on the 'play' button. Click several times, while the sound is playing."
+        + "Note that the sound doesn't always play from the beginning. Sometimes it plays from somewhere in the middle.</div>");
+    c.setScript("function play(id) {"
+        //
+        + "var e = document.getElementById(id);"
+        + "try {e.pause();} catch(e) {alert(\"pause: \" + e);}"
+        + "try {e.currentTime = 0;} catch(e) {alert(\"currentTime: \" + e);}"
+        + "try {e.play();} catch(e) {alert(\"play: \" + e);}"
+        //
+        + "}");
     return c;
   }
 
