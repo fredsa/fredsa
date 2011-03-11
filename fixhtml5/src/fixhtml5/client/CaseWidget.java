f262c163222972612e837d93b0e696a3db58aeed
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-11 21:27:14 +0000

    harcoded test case

--- fixhtml5/src/fixhtml5/client/CaseWidget.java
+++ fixhtml5/src/fixhtml5/client/CaseWidget.java
@@ -2,36 +2,48 @@ package fixhtml5.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.PreElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Widget;
 
 import fixhtml5.shared.Case;
 
 public class CaseWidget extends Composite {
 
-  private static CaseWidgetUiBinder uiBinder = GWT.create(CaseWidgetUiBinder.class);
-
   interface CaseWidgetUiBinder extends UiBinder<Widget, CaseWidget> {
   }
 
-  @UiField
-  Element caseTitle;
+  private static CaseWidgetUiBinder uiBinder = GWT.create(CaseWidgetUiBinder.class);
 
   @UiField
   Button button;
 
   @UiField
-  PreElement pre;
+  Element caseTitle;
+
+  @UiField
+  TextArea htmlTextArea;
+
+  @UiField
+  TextArea scriptTextArea;
+
+  @UiField
+  HTML result;
 
   public CaseWidget(Case c) {
     initWidget(uiBinder.createAndBindUi(this));
     caseTitle.setInnerHTML(c.getName());
+    htmlTextArea.setValue(c.getHtml());
+    scriptTextArea.setValue(c.getScript());
+    result.setHTML(c.getHtml());
+    eval(c.getScript());
+
     button.addClickHandler(new ClickHandler() {
 
       @Override
@@ -41,5 +53,9 @@ public class CaseWidget extends Composite {
       }
     });
   }
+
+  private native void eval(String script)/*-{
+    $wnd.eval(script);
+  }-*/;
 
 }
