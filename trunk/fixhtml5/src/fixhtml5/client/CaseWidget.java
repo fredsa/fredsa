e0374d7e593bb4a96e569c89e63919f0bbdee433
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-11 20:30:55 +0000

    start displaying first case

--- trunk/fixhtml5/src/fixhtml5/client/CaseWidget.java
+++ trunk/fixhtml5/src/fixhtml5/client/CaseWidget.java
@@ -2,25 +2,44 @@ package fixhtml5.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.PreElement;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.UIObject;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
 
 import fixhtml5.shared.Case;
 
-public class CaseWidget extends UIObject {
+public class CaseWidget extends Composite {
 
-  private static CaseUiBinder uiBinder = GWT.create(CaseUiBinder.class);
+  private static CaseWidgetUiBinder uiBinder = GWT.create(CaseWidgetUiBinder.class);
 
-  interface CaseUiBinder extends UiBinder<Element, CaseWidget> {
+  interface CaseWidgetUiBinder extends UiBinder<Widget, CaseWidget> {
   }
 
   @UiField
   Element caseTitle;
 
+  @UiField
+  Button button;
+
+  @UiField
+  PreElement pre;
+
   public CaseWidget(Case c) {
-    setElement(uiBinder.createAndBindUi(this));
+    initWidget(uiBinder.createAndBindUi(this));
     caseTitle.setInnerHTML(c.getName());
+    button.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        // TODO Auto-generated method stub
+
+      }
+    });
   }
 
 }
