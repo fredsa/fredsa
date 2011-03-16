687aeb7d098a65d6b98aa7e7c5501f3b5008c106
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-16 06:05:29 +0000

    better loading

---
+++ pda2/load.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function csv() {
+  kind=$1
+  upper=$(echo $1 | tr '[a-z]' '[A-Z]')
+  rm -f $kind.csv
+  cat pda.csv | grep $upper > temp
+  row=$(cat temp | head -1)
+  (echo "$row"; cat temp | grep -v "$row") > $kind.csv
+  rm temp
+  appcfg.py upload_data \
+            --url=http://pda2.ext.allen-sauer.com/_ah/remote_api \
+            --kind=$upper \
+            --config_file=bulkloader.yaml --file=$kind.csv \
+            --email=archer@allen-sauer.com \
+            --batch_size=50 \
+            --rps_limit=100 \
+            --num_threads=20 \
+            .
+  ls -l $kind.csv
+}
+
+for kind in person contact address calendar
+do
+  csv $kind
+done
