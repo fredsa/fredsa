de3d888b73b9ed9d6150e0b3fbd7ccc3091d510f
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-17 05:02:29 +0000

    mapreduce

--- pda2/main.py
+++ pda2/main.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import logging
+import pprint
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 from google.appengine.api import users
-
-import pprint
+from mapreduce import operation as op
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
@@ -138,6 +126,9 @@ Search text: <input type="text" name="q" value=""> <input type="submit" value="G
 class SelectableStringProperty(db.StringProperty):
   pass
 
+class PERSON(db.Expando):
+  pass
+
 class Person(db.Model):
   mailing_name = db.StringProperty(verbose_name="Mailing Name", default="")
   title = db.StringProperty(verbose_name="Title", default="")
@@ -163,6 +154,12 @@ class Person(db.Model):
       return self.key()
     else:
       return ""
+
+
+def migrate(entity):
+ person = Person(key_name=entity.key().name())
+ logging.info(person)
+ yield op.db.Put(person)
 
 
 def main():
