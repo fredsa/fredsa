c79a2543f0ed2d22f5aaa85dd05e2ae08407f447
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-18 22:51:54 +0000

    Initial import.

--- trunk/filemanager/src/filemanager/client/Filemanager.java
+++ trunk/filemanager/src/filemanager/client/Filemanager.java
@@ -1,8 +1,12 @@
 package filemanager.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Filemanager implements EntryPoint {
+  @Override
   public void onModuleLoad() {
+    RootPanel.get().add(new Label("Hi"));
   }
 }
