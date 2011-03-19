b8ac1d575777f01d6d710c5cc508ee869380eb5a
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-19 05:37:46 +0000

    use memcache

--- listen2spell/src/listen2spell/server/WordServlet.java
+++ listen2spell/src/listen2spell/server/WordServlet.java
@@ -8,6 +8,8 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Text;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.appengine.api.urlfetch.HTTPResponse;
 import com.google.appengine.api.urlfetch.URLFetchService;
 import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
@@ -35,29 +37,36 @@ public class WordServlet extends RemoteServiceServlet implements WordService {
     word = map(word.toLowerCase());
     DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
     URLFetchService fs = URLFetchServiceFactory.getURLFetchService();
+    MemcacheService ms = MemcacheServiceFactory.getMemcacheService();
     try {
       Entity entity;
       String dataUrl;
-      try {
-        Log.debug("get()...");
-        entity = ds.get(KeyFactory.createKey(KIND, word));
-        dataUrl = ((Text) entity.getProperty("dataUrl")).getValue();
-      } catch (EntityNotFoundException e) {
-        URL url = new URL("http://translate.google.com/translate_tts?ie=UTF-8&tl=en&prev=input&q="
-            + word);
-        Log.debug("fetch(" + url + ")...");
-        HTTPResponse resp = fs.fetch(url);
-        byte[] content = resp.getContent();
-        dataUrl = "data:audio/mpeg;base64," + Base64.encode(content);
+      Word w;
+      Log.debug("memcache get()...");
+      w = (Word) ms.get(word);
+      if (w == null) {
+        try {
+          Log.debug("datastore get()...");
+          entity = ds.get(KeyFactory.createKey(KIND, word));
+          dataUrl = ((Text) entity.getProperty("dataUrl")).getValue();
+        } catch (EntityNotFoundException e) {
+          URL url = new URL("http://translate.google.com/translate_tts?ie=UTF-8&tl=en&prev=input&q="
+              + word);
+          Log.debug("url fetch(" + url + ")...");
+          HTTPResponse resp = fs.fetch(url);
+          byte[] content = resp.getContent();
+          dataUrl = "data:audio/mpeg;base64," + Base64.encode(content);
 
-        entity = new Entity(KIND, word);
-        entity.setProperty("word", word);
-        entity.setUnindexedProperty("dataUrl", new Text(dataUrl));
-        Log.debug("put()...");
-        ds.put(entity);
+          entity = new Entity(KIND, word);
+          entity.setProperty("word", word);
+          entity.setUnindexedProperty("dataUrl", new Text(dataUrl));
+          Log.debug("datastore put()...");
+          ds.put(entity);
+        }
+        w = new Word(word, dataUrl);
+        ms.put(word, w);
       }
 
-      Word w = new Word(word, dataUrl);
       return new Word[] {w};
     } catch (Exception e) {
       Log.error("oops", e);
