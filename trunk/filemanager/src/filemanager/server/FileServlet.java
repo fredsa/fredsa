4ba4643872dfdebbc13d523860ac851f32438629
fredsa@google.com <fredsa@google.com@3e68d9b2-293c-11de-8f4a-8f4f67deb9c4>
2011-03-23 00:50:23 +0000

    prevent existing blobs from being orphaned

--- trunk/filemanager/src/filemanager/server/FileServlet.java
+++ trunk/filemanager/src/filemanager/server/FileServlet.java
@@ -5,7 +5,6 @@ import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.appengine.api.datastore.Blob;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
